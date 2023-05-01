d19443e7409bdea75918c614a5d10f2364b7fffc
Tfanios <63114358+Tfanios@users.noreply.github.com>
2023-05-01 23:37:35 +0300

    minor changes

---
+++ todo-app/src/components/TodoListComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import {ref, onMounted, computed, watch} from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+onMounted(()=>{
+  name.value = localStorage.getItem('name') || ''
+})
+
+
+</script>
+
+<template>
+    <h1>List of todos</h1>
+</template>
+
+<style scoped>
+
+</style>
